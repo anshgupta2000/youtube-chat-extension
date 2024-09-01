15f565ae17b9140b084b1d95c54ae6eeec6f6fba
Ansh Gupta <48497677+anshgupta2000@users.noreply.github.com>
2024-09-01 16:54:19 -0400

    Delete backend directory

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const captionsRouter = require('./captions');
-
-router.use('/captions', captionsRouter);
-
-module.exports = router;
